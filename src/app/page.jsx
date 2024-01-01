98ac3bc2b5803eda52aa1ae1fb464de3f3bf294b
Tanvir Ahamed Sowmik <111014007+sowmik265@users.noreply.github.com>
2024-01-01 13:30:42 +0600

    home page

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,5 +1,35 @@
+import Image from "next/image";
+import styles from "./home.module.css";
+
 const Home = () => {
-  return <div>Home page</div>;
+  return (
+    <div className={styles.container}>
+      <div className={styles.textContainer}>
+        <h1 className={styles.title}>Creative Thoughts Agency.</h1>
+        <p className={styles.desc}>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Similique
+          repellat id sequi perspiciatis voluptatibus aliquid reprehenderit
+          quasi dicta. Voluptas, placeat?
+        </p>
+        <div className={styles.buttons}>
+          <button className={styles.button}>Learn More</button>
+          <button className={styles.button}>Contact</button>
+        </div>
+        <div className={styles.brands}>
+          <Image
+            src="/brands.png"
+            alt=""
+            fill
+            className={styles.brandImg}
+          ></Image>
+        </div>
+      </div>
+
+      <div className={styles.imgContainer}>
+        <Image src="/hero.gif" alt="" fill className={styles.heroImg}></Image>
+      </div>
+    </div>
+  );
 };
 
 export default Home;
