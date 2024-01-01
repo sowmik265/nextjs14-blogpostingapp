3eda5ea91cb6226c4ad6d56bb4a5f66fff51e81e
Tanvir Ahamed Sowmik <111014007+sowmik265@users.noreply.github.com>
2024-01-01 11:31:26 +0600

    navbar & media query done

--- src/components/navbar/links/Links.jsx
+++ src/components/navbar/links/Links.jsx
@@ -1,33 +1,67 @@
-import Link from "next/link";
+"use client";
+import { useState } from "react";
 import styles from "./links.module.css";
+import NavLink from "./navlink/NavLink";
+
+const links = [
+  {
+    title: "Home",
+    path: "/",
+  },
+  {
+    title: "About",
+    path: "/about",
+  },
+  {
+    title: "Contact",
+    path: "/contact",
+  },
+  {
+    title: "Blog",
+    path: "/blog",
+  },
+];
 
 const Links = () => {
-  const links = [
-    {
-      title: "Home",
-      path: "/",
-    },
-    {
-      title: "About",
-      path: "/about",
-    },
-    {
-      title: "Contact",
-      path: "/contact",
-    },
-    {
-      title: "Blog",
-      path: "/blog",
-    },
-  ];
+  const [open, setOpen] = useState(false);
+
+  const session = true;
+  const isAdmin = true;
 
   return (
-    <div className={styles.links}>
-      {links.map((link) => (
-        <Link href={link.path} key={link.title}>
-          {link.title}
-        </Link>
-      ))}
+    <div className={styles.container}>
+      {/* large device menu */}
+      <div className={styles.links}>
+        {links.map((link) => (
+          <NavLink item={link} key={link.title}></NavLink>
+        ))}
+        {session ? (
+          <>
+            {isAdmin && (
+              <NavLink item={{ title: "Admin", path: "/admin" }}></NavLink>
+            )}
+            <button className={styles.logout}>Logout</button>
+          </>
+        ) : (
+          <NavLink item={{ title: "Login", path: "/login" }}></NavLink>
+        )}
+      </div>
+      {/* end */}
+      {/* small device menu */}
+      <button
+        className={styles.menuButton}
+        onClick={() => setOpen((prev) => !prev)}
+      >
+        Menu
+      </button>
+      {open && (
+        <div className={styles.mobileLinks}>
+          {links.map((link) => (
+            <NavLink item={link} key={link.title}></NavLink>
+          ))}
+        </div>
+      )}
+      {/* end */}
     </div>
   );
 };
---
+++ src/components/navbar/links/navlink/NavLink.jsx
@@ -0,0 +1,20 @@
+"use client";
+import Link from "next/link";
+import { usePathname } from "next/navigation";
+import styles from "./navlink.module.css";
+const NavLink = ({ item }) => {
+  const pathName = usePathname();
+  // console.log(pathName);
+  return (
+    <Link
+      href={item.path}
+      className={`${styles.container} ${
+        pathName === item.path && styles.active
+      }`}
+    >
+      {item.title}
+    </Link>
+  );
+};
+
+export default NavLink;
