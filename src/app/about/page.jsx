073da7d4883ccb0954291aaa75e50fc20b20290c
Tanvir Ahamed Sowmik <111014007+sowmik265@users.noreply.github.com>
2024-01-01 14:15:05 +0600

    about

--- src/app/about/page.jsx
+++ src/app/about/page.jsx
@@ -3,9 +3,37 @@ import styles from "./about.module.css";
 
 const About = () => {
   return (
-    <div>
+    <div className={styles.container}>
+      <div className={styles.textContainer}>
+        <h2 className={styles.subtitle}>Lorem, ipsum.</h2>
+        <h1 className={styles.title}>
+          Lorem ipsum dolor sit, amet consectetur adipisicing elit. Dolorum,
+          suscipit sequi.
+        </h1>
+        <p className={styles.desc}>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Qui
+          laudantium minima impedit cum commodi atque, sit modi optio dicta
+          possimus esse amet officiis tempore sint fugit pariatur laboriosam
+          similique nobis repellendus ipsa voluptate. Iusto eaque at sunt atque
+          molestias ad autem dolor officia in quasi?
+        </p>
+        <div className={styles.boxes}>
+          <div className={styles.box}>
+            <h1>10 +</h1>
+            <p>Year of experience</p>
+          </div>
+          <div className={styles.box}>
+            <h1>10 +</h1>
+            <p>Year of experience</p>
+          </div>
+          <div className={styles.box}>
+            <h1>10 +</h1>
+            <p>Year of experience</p>
+          </div>
+        </div>
+      </div>
       <div className={styles.imgContainer}>
-        <Image src="/about.png" alt="" fill></Image>
+        <Image src="/about.png" alt="" fill className={styles.img}></Image>
       </div>
     </div>
   );
