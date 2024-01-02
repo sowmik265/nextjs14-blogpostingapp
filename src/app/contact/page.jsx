0dc584923e72b9db1dbf9872fa856ca33613628e
Tanvir Ahamed Sowmik <111014007+sowmik265@users.noreply.github.com>
2024-01-02 10:50:48 +0600

    contact page

--- src/app/contact/page.jsx
+++ src/app/contact/page.jsx
@@ -1,5 +1,23 @@
+import Image from "next/image";
+import styles from "./contact.module.css";
+
 const Contact = () => {
-  return <div>Contact</div>;
+  return (
+    <div className={styles.container}>
+      <div className={styles.imgContainer}>
+        <Image src="/contact.png" alt="" fill className="styles.img"></Image>
+      </div>
+      <div className={styles.formContainer}>
+        <form action="" className={styles.form}>
+          <input type="text" placeholder="Full name" />
+          <input type="email" placeholder="Email address" />
+          <input type="number" placeholder="Phone number (Optional)" />
+          <textarea name="" id="" cols="30" rows="10" placeholder="Message"></textarea>
+          <button>Send</button>
+        </form>
+      </div>
+    </div>
+  );
 };
 
 export default Contact;
