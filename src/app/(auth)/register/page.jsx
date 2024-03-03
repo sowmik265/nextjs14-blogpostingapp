06f3aa580295aaf6718363686c26d1c49a4c16bd
Tanvir Ahamed Sowmik <111014007+sowmik265@users.noreply.github.com>
2024-03-03 10:51:58 +0600

    user credential/register

--- src/app/(auth)/register/page.jsx
+++ src/app/(auth)/register/page.jsx
@@ -1,5 +1,23 @@
+import { handleRegister } from "@/lib/action";
+import styles from "./register.module.css";
 const Register = () => {
-  return <div>Register</div>;
+  return (
+    <div className={styles.container}>
+      <div className={styles.wrapper}>
+        <form className={styles.form} action={handleRegister}>
+          <input type="text" placeholder="User name" name="userName" />
+          <input type="email" placeholder="email" name="email" />
+          <input type="password" placeholder="password" name="password" />
+          <input
+            type="password"
+            placeholder="password again"
+            name="passwordRepeat"
+          />
+          <button>Register</button>
+        </form>
+      </div>
+    </div>
+  );
 };
 
 export default Register;
--- src/lib/action.js
+++ src/lib/action.js
@@ -3,7 +3,7 @@
 import { revalidatePath } from "next/cache";
 import { signIn, signOut } from "./auth";
 import { connectToDb } from "./connectToDb";
-import { Post } from "./models";
+import { Post, User } from "./models";
 
 export const addPost = async (formData) => {
   const { title, desc, slug, userId } = Object.fromEntries(formData);
@@ -29,6 +29,32 @@ export const deletePost = async (formData) => {
   } catch (error) {
     console.log(error);
     return { error: "something went wrong!" };
+  }
+};
+
+export const handleRegister = async (formData) => {
+  const { userName, email, password, passwordRepeat } =
+    Object.fromEntries(formData);
+  if (password !== passwordRepeat) {
+    return "password does not match";
+  }
+  try {
+    connectToDb();
+    const user = await User.findOne({ email });
+    if (user) {
+      return "user already exist !";
+    }
+    const newUser = new User({
+      userName,
+      email,
+      password,
+      passwordRepeat,
+    });
+    await newUser.save();
+    console.log("saved to DB ! ");
+  } catch (error) {
+    console.log(error);
+    return { error: "something went wrong !" };
   }
 };
 
